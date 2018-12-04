9cb2496fc961921f1fa9bc46358738658e3d01bd
CodeyWorley <codeyworley@gmail.com>
2018-12-04 12:04:55 -0700

    fixed env variables

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://admin:password1@ds155252.mlab.com:55252/noteful',
-  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://admin:password1@ds155252.mlab.com:55252/noteful',
+  MONGODB_URI: process.env.MONGODB_URI,
+  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 };
